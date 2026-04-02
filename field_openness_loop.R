059b4b41567ccf08a04179e54eefe4dc4736717b
Kevin Tolan <82405167+kevtolan@users.noreply.github.com>
2026-04-01 22:00:28 -0400

    Update field_openness_loop.R

--- field_openness_loop.R
+++ field_openness_loop.R
@@ -23,19 +23,31 @@ library(classInt)
 library(rnaturalearth)
 library(rgrass)
 library(cowplot)
-
 td <- tempdir()
 out_dir <- '~/R/Grasslab hab/LiDAR_Loop'
-# terraOptions(progress = 0) # prevent progress bars during raster functions
 
-lake <- st_read('~/R/Grasslab hab/spat/VT_Lake_Champlain_(extracted_from_VHDCARTO)_-_polygon.shp') %>%
-  st_transform(32145)
-# ====================
-# Define area of interest
-# ====================
+temp_zip22 <- tempfile(fileext = ".zip")
+
+download.file("https://s3.us-east-2.amazonaws.com/vtopendata-prd/Landcover/_Packaged_Zips/LandLandcov_Agriculture2022.zip",
+              destfile = temp_zip22, mode = "wb")
+unzip(temp_zip22, exdir = td)
+
+VT_hay_pasture_22 <- st_read(dsn = paste0(td, "/LandLandcov_Agriculture2022/LandLandcov_Agriculture2022.gdb"),
+                             layer = "LandLandcov_Agriculture2022_poly") %>%
+  st_transform(crs = 32145) %>% st_make_valid() %>%
+  dplyr::filter(Class != 'Crops') %>%
+  mutate(area_ha = Shape_Area/10000,
+         area_ac = Shape_Area*0.000247105,
+         perim_area = Shape_Length/Shape_Area,
+         sourceyear = '2022') %>%
+  filter(perim_area <= 0.03,
+         area_ac >= 10) %>%
+  st_simplify(dTolerance = 1, preserveTopology = T) %>%
+  dplyr::select(!c('gridcode'))
+
+
 vt_blocks <- get_spatial_layer("https://services1.arcgis.com/d3OaJoSAh2eh6OA9/ArcGIS/rest/services/Vermont_Wildlife_Atlasing_Blocks/FeatureServer/0",
                                out_fields = c("BLOCKNAME","QUADNAME","GEOUNITDES")) %>% st_transform(crs = 32145) # download grid
-
 
 aoi <- vt_blocks
 files <- list.files(out_dir, pattern = "\\.tif$")
@@ -50,38 +62,57 @@ dsm_cog_url <- dsm_cog_url_central
 raster_extent <- ext(dsm_cog_url)
 bbox_poly <- as.polygons(raster_extent, crs = crs(dsm_cog_url)) %>% project("EPSG:32145")
 cropped_blocks <- st_crop(vt_blocks, bbox_poly)
+bb <- st_bbox(cropped_blocks)
 
-blocks <- unique(cropped_blocks$BLOCKNAME)
+width <- bb[["xmax"]] - bb[["xmin"]]
+western_cutoff <- bb[["xmin"]] + (width * 0.25)
+
+west_coords <- matrix(c(
+  bb[["xmin"]], bb[["ymin"]],
+  western_cutoff, bb[["ymin"]],
+  western_cutoff, bb[["ymax"]],
+  bb[["xmin"]], bb[["ymax"]],
+  bb[["xmin"]], bb[["ymin"]]),
+  ncol = 2, byrow = TRUE)
+
+subset_poly <- st_sfc(st_polygon(list(west_coords)), crs = st_crs(cropped_blocks))
+
+blocks_subset <- cropped_blocks[st_intersects(cropped_blocks, subset_poly, sparse = FALSE), ]
+
+blocks <- unique(blocks_subset$BLOCKNAME)
 total_blocks <- length(blocks)
-processed_count <- 0  # Track actual file ran
-skipped_count <- 0    # Track skipped files that already exist
-
-first_block <- aoi[aoi$BLOCKNAME == blocks[1], ]
-dsm_init <- crop(dsm_cog_url, first_block) %>% project("EPSG:32145", method = "bilinear")
-loc <- initGRASS(gisBase = '/Applications/GRASS-8.2.app/Contents/Resources',
-                 home = td,
-                 SG = dsm_init,
-                 override = TRUE)
+processed_count <- 0
+skipped_count <- 0
 
 beginCluster()
 
-script_start_time <- Sys.time() # Master timer
+script_start_time <- Sys.time()
 
 for (counter in seq_along(blocks)) {
   i <- blocks[counter]
-  iteration_start_time <- Sys.time() # Timer for just this iteration
-  out_file <- file.path(out_dir, paste0(i, "_open.tif"))  # <-- add this
+  iteration_start_time <- Sys.time()
+  out_file <- file.path(out_dir, paste0(i, "_open.tif"))
 
-  if (file.exists(out_file)) { # skip spatial object if its file already exits in directory
+  if (file.exists(out_file)) {
     cli_alert_info("Skipping block: {.val {i}} (file already exists)")
     skipped_count <- skipped_count + 1
     next }
 
-  tryCatch({ # reduce the chance of the whole loop stopping should one town fail
+  blockbound <- aoi[aoi$BLOCKNAME == i, ]
+
+  hay_block <- tryCatch(
+    st_crop(VT_hay_pasture_22, blockbound),
+    error = function(e) { sf::st_sf(geometry = sf::st_sfc(crs = st_crs(VT_hay_pasture_22))) }
+  )
+
+  if (nrow(hay_block) == 0) {
+    cli_alert_warning("No hay/pasture in block {.val {i}} — skipping")
+    skipped_count <- skipped_count + 1
+    next
+  }
+
+  tryCatch({
     blockbound <- aoi[aoi$BLOCKNAME == i, ]
-
-
-# dsm_cog <- crop(dsm_cog_url, blockbound) %>% project("EPSG:32145", method = "bilinear")
 
 dsm_cog <- tryCatch(
   crop(dsm_cog_url, blockbound) %>% project("EPSG:32145", method = "bilinear"),
@@ -91,30 +122,32 @@ dsm_cog <- tryCatch(
   }
 )
 
-# loc <- initGRASS(gisBase= '/Applications/GRASS-8.2.app/Contents/Resources',
-#                  home = td,
-#                  SG = dsm_cog,
-#                  override = TRUE)
-write_RAST(dsm_cog, vname="rmask")
+loc <- initGRASS(gisBase = '/Applications/GRASS-8.2.app/Contents/Resources',
+                 home = td,
+                 SG = dsm_cog,
+                 override = TRUE)
+
+write_RAST(dsm_cog, vname = "rmask", flags = "overwrite")
 
 execGRASS("r.skyview", flags=c("o","overwrite"),
           input= 'rmask', output = 'rmask.Open')
 
 u1 <- read_RAST('rmask.Open')
 
-lake_block <- tryCatch(
-  st_crop(lake, blockbound),
-  error = function(e) { sf::st_sf(geometry = sf::st_sfc(crs = st_crs(lake))) }
-)
-
-if (nrow(lake_block) > 0) {
-  lake_vect <- vect(lake_block)
-  u1 <- mask(u1, lake_vect, inverse = TRUE)
-}
+hay_vect <- vect(hay_block)
+u1 <- mask(u1, hay_vect)
 
 
-# u1[u1[] >= 1 ] <- NA
-u1[u1[] <= .8 ] <- NA
+# lake_block <- tryCatch(
+#   st_crop(lake, blockbound),
+#   error = function(e) { sf::st_sf(geometry = sf::st_sfc(crs = st_crs(lake))) }
+# )
+#
+# if (nrow(lake_block) > 0) {
+#   lake_vect <- vect(lake_block)
+#   u1 <- mask(u1, lake_vect, inverse = TRUE)
+# }
+
 
 writeRaster(u1, paste0(out_dir,"/",i,"_open.tif"), overwrite = T)
 
